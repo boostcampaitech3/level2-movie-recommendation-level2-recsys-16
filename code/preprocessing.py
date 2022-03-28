575b419aff25e6df728c7196f9cffac89bc564aa
ler0n <lyint.kor@gmail.com>
2022-03-28 22:27:43 +0900

    :sparkles: Add baseline codes

---
+++ code/preprocessing.py
@@ -0,0 +1,18 @@
+import pandas as pd
+import numpy as np
+
+def main():
+    genres_df = pd.read_csv("../data/train/genres.tsv", sep="\t")
+    array, index = pd.factorize(genres_df["genre"])
+    genres_df["genre"] = array
+    genres_df.groupby("item")["genre"].apply(list).to_json(
+        "../data/Ml_item2attributes.json"
+    )
+    
+    item_df = pd.read_csv('../data/train/titles.tsv', sep='\t')
+    item_ids = item_df['item'].unique()
+    item2idx = pd.Series(data=np.arange(len(item_ids))+1, index=item_ids)
+    item2idx.to_csv('../data/item2idx.tsv', sep='\t', encoding='utf-8', index=True)
+
+if __name__ == "__main__":
+    main()
