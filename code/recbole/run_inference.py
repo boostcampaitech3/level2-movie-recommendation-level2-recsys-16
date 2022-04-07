60c8316735dceeb65f63cc137e5d79e88ac15ec3
ler0n <lyint.kor@gmail.com>
2022-04-07 14:32:43 +0900

    :sparkles: Add recbole inference.py

---
+++ code/recbole/run_inference.py
@@ -0,0 +1,79 @@
+# @Time   : 2020/7/20
+# @Author : Shanlei Mu
+# @Email  : slmu@ruc.edu.cn
+
+# UPDATE
+# @Time   : 2020/10/3, 2020/10/1
+# @Author : Yupeng Hou, Zihan Lin
+# @Email  : houyupeng@ruc.edu.cn, zhlin@ruc.edu.cn
+
+
+import argparse
+import torch
+import numpy as np
+import pandas as pd
+
+from recbole.quick_start import load_data_and_model
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--model_path', '-m', type=str, default='saved/model.pth', help='name of models')
+    
+    args, _ = parser.parse_known_args()
+    
+    # model, dataset 불러오기
+    config, model, dataset, train_data, valid_data, test_data = load_data_and_model(args.model_path)
+    
+    # device 설정
+    device = config.final_config_dict['device']
+    
+    # user, item id -> token 변환 array
+    user_id2token = dataset.field2id_token['user_id']
+    item_id2token = dataset.field2id_token['item_id']
+    
+    # user-item sparse matrix
+    matrix = dataset.inter_matrix(form='csr')
+
+    # user id, predict item id 저장 변수
+    pred_list = None
+    user_list = None
+    
+    model.eval()
+    for data in test_data:
+        interaction = data[0].to(device)
+        score = model.full_sort_predict(interaction)
+        
+        rating_pred = score.cpu().data.numpy().copy()
+        batch_user_index = interaction['user_id'].cpu().numpy()
+        rating_pred[matrix[batch_user_index].toarray() > 0] = 0
+        ind = np.argpartition(rating_pred, -10)[:, -10:]
+        
+        arr_ind = rating_pred[np.arange(len(rating_pred))[:, None], ind]
+
+        arr_ind_argsort = np.argsort(arr_ind)[np.arange(len(rating_pred)), ::-1]
+
+        batch_pred_list = ind[
+            np.arange(len(rating_pred))[:, None], arr_ind_argsort
+        ]
+        
+        # 예측값 저장
+        if pred_list is None:
+            pred_list = batch_pred_list
+            user_list = batch_user_index
+        else:
+            pred_list = np.append(pred_list, batch_pred_list, axis=0)
+            user_list = np.append(user_list, batch_user_index, axis=0)
+        
+    result = []
+    for user, pred in zip(user_list, pred_list):
+        for item in pred:
+            result.append((int(user_id2token[user]), int(item_id2token[item])))
+            
+    # 데이터 저장
+    dataframe = pd.DataFrame(result, columns=["user", "item"])
+    dataframe.sort_values(by='user', inplace=True)
+    dataframe.to_csv(
+        "submission.csv", index=False
+    )
+    print('inference done!')
