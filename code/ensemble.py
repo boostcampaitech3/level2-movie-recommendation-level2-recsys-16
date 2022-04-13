c31b07979be7e600d19e3edec1117813d2f07f2c
ler0n <lyint.kor@gmail.com>
2022-04-13 18:38:50 +0900

    :sparkles: Add rank ratio

--- code/ensemble.py
+++ code/ensemble.py
@@ -1,9 +1,14 @@
+import sys
 import pandas as pd
 from collections import defaultdict
 from tqdm import tqdm
 
 ratios = []
 dataframe_list = []
+
+print('순위별 가중치값 입력(ex: 1 0.9 0.8 ...)')
+rank_ratio = list(map(float, sys.stdin.readline().split()))
+rank_len = len(rank_ratio)
 
 num = int(input('앙상블할 모델 결과 개수: '))
 
@@ -20,14 +25,14 @@ result = []
 tbar = tqdm(user_list, desc='Ensemble')
 for user in tbar:
     temp = defaultdict(float)
-    for idx in range(dataframe_len):
-        items = dataframe_list[idx][dataframe_list[idx]['user'] == user]['item'].values
-        for item in items:
-            temp[item] += ratios[idx]
+    for df_idx in range(dataframe_len):
+        items = dataframe_list[df_idx][dataframe_list[df_idx]['user'] == user]['item'].values
+        max_rank = min(len(items), rank_len)
+        for rank_idx in range(max_rank):
+            temp[items[rank_idx]] += rank_ratio[rank_idx] * ratios[df_idx]
 
     for key, _ in sorted(temp.items(), key=lambda x: x[1], reverse=True)[:10]:
         result.append((user, key))
 
 submission = pd.DataFrame(result, columns=['user', 'item'])
-
-submission.to_csv(f'ensemble-{num}file', index=False)
+submission.to_csv(f'ensemble({num})-file.csv', index=False)
